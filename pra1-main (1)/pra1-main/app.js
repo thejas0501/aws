f9357f9d547a0e1695946a25e8afb2f0d742b90f
thejas_0501 <84975127+thejas0501@users.noreply.github.com>
2024-06-28 21:15:33 +0530

    Add files via upload

---
+++ pra1-main (1)/pra1-main/app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+app.use(express.static(__dirname + '/public'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`API server listening on port ${port}`);
+});
